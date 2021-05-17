0d2811b179b5594ebf4408449df7e3d8c71a3ee4
Josef Sima <josef.sima@topite.cz>
2021-05-17 16:00:59 +0200

    wip

---
+++ src/Shared/Utils/Constants.lua
@@ -0,0 +1,6 @@
+local Constants = {
+  LEADER_STATS = "leaderstats",
+  BUTTON_BACKGROUND_COLOR3 = Color3.fromRGB(23, 26, 255)
+}
+
+return Constants
--- src/StarterGui/Lobby/GamesButton/GamesButton.client.lua
+++ src/StarterGui/Lobby/GamesButton/GamesButton.client.lua
@@ -1,7 +1,28 @@
-local gamesButton = script.Parent:WaitForChild('Screen'):WaitForChild('GamesButton')
+repeat wait() until game.Players.LocalPlayer.Character
+local C = require(game.ReplicatedStorage:WaitForChild('Utils'):WaitForChild('Constants'))
 
 local function onButtonActivated()
 	print("button pressed!")
 end
 
+local Players = game:GetService('Players')
+local playerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
+
+-- create screen gui
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = playerGui
+screenGui.Enabled = true
+
+-- create button
+local gamesButton = Instance.new("TextButton")
+gamesButton.Parent = screenGui
+gamesButton.Text = "Games"
+gamesButton.Font = "Arcade"
+gamesButton.Size = UDim2.new(0.2, 0, 0.05, 0)
+gamesButton.Position = UDim2.new(0.4, 0, 0.025, 0)
+gamesButton.TextScaled = true
+gamesButton.BackgroundColor3 = C.BUTTON_BACKGROUND_COLOR3
+gamesButton.BorderColor3 = Color3.fromRGB(0, 0, 163)
+gamesButton.BorderSizePixel = 10
+gamesButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 gamesButton.Activated:Connect(onButtonActivated)
