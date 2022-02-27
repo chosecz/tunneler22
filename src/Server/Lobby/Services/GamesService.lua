19b4d8c03270ebacd28f718345770da62796af0e
Josef Šíma <ja@chose.cz>
2022-02-27 13:50:08 +0100

    GamesService update

--- src/Server/Lobby/Services/GamesService.lua
+++ src/Server/Lobby/Services/GamesService.lua
@@ -1,7 +1,9 @@
+local C = require(game.ReplicatedStorage:WaitForChild('Utils'):WaitForChild('Constants'))
 local remoteFunctions = game:GetService('ReplicatedStorage'):WaitForChild('RemoteFunctions')
 local GamesService = {}
 
 GamesService.PublicGames = {}
+GamesService.FriendGames = {}
 
 local function generateGameId(userId)
    return "GID_"..userId
@@ -13,15 +15,21 @@ end
 
 function GamesService.CreateGame(player, options)
    print("Server: CreateGame", options)
+   local gameId = generateGameId(player.UserId);
    local game = {
       GameType = options.GameType,
       GameMode = options.GameMode,
       Owner = player
    }
-   local gameId = generateGameId(player.UserId);
-   GamesService.PublicGames[gameId] = game
-   print('gameId', gameId)
-   print('game', game)
+
+   -- is public or friend game
+   if options.GameType == C.GAME_TYPE.PUBLIC then
+      GamesService.PublicGames[gameId] = game
+   elseif options.GameType == C.GAME_TYPE.FRIEND then
+      GamesService.FriendGames[gameId] = game
+   end
+
+   print("Server: Game created", gameId)
    return gameId
 end
 
