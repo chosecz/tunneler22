8a1bcb3e03375d8111f43280c04abbbdf14262bb
Josef Šíma <ja@chose.cz>
2022-04-15 18:37:20 +0200

    TeleportScreen

--- src/StarterGui/Lobby/TeleportScreen.client.lua
+++ src/StarterGui/Lobby/TeleportScreen.client.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until game.Players.LocalPlayer.Character
+
 local TeleportService = game:GetService("TeleportService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
@@ -8,11 +10,12 @@ local playerGui = player:WaitForChild("PlayerGui")
 -- Get remote event created by the teleport module
 local teleportEvent = ReplicatedStorage:WaitForChild("TeleportEvent")
 
-local screenGui = Instance.new("ScreenGui", playerGui)
-screenGui.IgnoreGuiInset = true
-screenGui.Enabled = false
+local teleportScreenGui = Instance.new("ScreenGui", playerGui)
+teleportScreenGui.IgnoreGuiInset = true
+teleportScreenGui.Enabled = false
+teleportScreenGui.Name = "TeleportScreen"
 
-local textLabel = Instance.new("TextLabel", screenGui)
+local textLabel = Instance.new("TextLabel", teleportScreenGui)
 textLabel.Size = UDim2.new(1, 0, 1, 0)
 textLabel.BackgroundColor3 = Color3.fromRGB(0, 20, 40)
 textLabel.Font = Enum.Font.GothamSemibold
@@ -21,11 +24,13 @@ textLabel.Text = "Teleporting..."
 textLabel.TextSize = 28
 
 -- Set the teleport GUI in preparation for teleport
-TeleportService:SetTeleportGui(screenGui)
+TeleportService:SetTeleportGui(teleportScreenGui)
 
 teleportEvent.OnClientEvent:Connect(function(playersTable, enable)
 	-- Enable or disable teleport GUI for valid players
 	if table.find(playersTable, player) then
-		screenGui.Enabled = enable
+		teleportScreenGui.Enabled = enable
 	end
 end)
+
+print("TeleportScreen ready")
