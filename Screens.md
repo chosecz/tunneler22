2922bc4992f72da9c0f763da65edfb42843f8573
Josef Šíma <ja@chose.cz>
2022-04-15 12:12:56 +0200

    Formats

--- Screens.md
+++ Screens.md
@@ -1,31 +1,17 @@
 Screens
 
 Lobby
+
 - StartScreen
-    - Games
-    - LeaderBoards
-        - Me
-        - Top players
-
-GameTypesScreen
-    - PublicGames
-    - FriendGames
-    - CreateGame
-
-MyGameScreen
-    - Leave
-    - Ready
-    - Teams
-        - 1 vs 1
-        - 2 vs 2
-
-LeaderBoards Screen
+  - Games
+  - LeaderBoards
     - Me
-    - Top Players
-        - Today
-        - Week
-        - Month
-        - Year
-        - Total
-    - ? :)
+    - Top players
+
+GameTypesScreen - PublicGames - FriendGames - CreateGame
+
+MyGameScreen - Leave - Ready - Teams - 1 vs 1 - 2 vs 2
+
+LeaderBoards Screen - Me - Top Players - Today - Week - Month - Year - Total - ? :)
+
 - ? :)
--- src/Server/Lobby/Services/GamesService.lua
+++ src/Server/Lobby/Services/GamesService.lua
@@ -1,180 +1,187 @@
-local C = require(game.ReplicatedStorage:WaitForChild('Utils'):WaitForChild('Constants'))
-local remoteFunctions = game:GetService('ReplicatedStorage'):WaitForChild('RemoteFunctions')
-local remoteEvents = game:GetService('ReplicatedStorage'):WaitForChild('RemoteEvents')
+local C = require(game.ReplicatedStorage:WaitForChild('Utils'):WaitForChild(
+                      'Constants'))
+local remoteFunctions = game:GetService('ReplicatedStorage'):WaitForChild(
+                            'RemoteFunctions')
+local remoteEvents = game:GetService('ReplicatedStorage'):WaitForChild(
+                         'RemoteEvents')
 local GamesService = {}
 
 -- holds list of all games
 local games = {}
 
 local function generateGameId(userId)
-   return "GID_"..userId.."_"..math.random(0,999)
+  return "GID_" .. userId .. "_" .. math.random(0, 999)
 end
 
 local function ListOfPublicGames()
-   -- TODO: filter only public games
-   return games
+  -- TODO: filter only public games
+  return games
 end
 
 local function combine(a1, a2)
-   local new = table.create(#a1 + #a2)
-   table.move(a1, 1, #a1, 1, new)
-   table.move(a2, 1, #a2, 1, new)
-   return new
+  local new = table.create(#a1 + #a2)
+  table.move(a1, 1, #a1, 1, new)
+  table.move(a2, 1, #a2, 1, new)
+  return new
 end
 
 local function updateGameStatus(game)
-   local redPlayers = game.Teams[C.GAME_TEAM.RED]
-   local bluePlayers = game.Teams[C.GAME_TEAM.BLUE]
-   local numberOfPlayers = #redPlayers + #bluePlayers
-   local allPlayers = {table.unpack(redPlayers),table.unpack(bluePlayers)}
+  local redPlayers = game.Teams[C.GAME_TEAM.RED]
+  local bluePlayers = game.Teams[C.GAME_TEAM.BLUE]
+  local numberOfPlayers = #redPlayers + #bluePlayers
+  local allPlayers = {table.unpack(redPlayers), table.unpack(bluePlayers)}
 
-   -- for GAME_MODE.ONE
-   local maxPlayers = 2
+  -- for GAME_MODE.ONE
+  local maxPlayers = 2
 
-   -- for GAME_MODE.TWO
-   if (game.GameMode == C.GAME_MODE.TWO) then
-      maxPlayers = 4
-   end
+  -- for GAME_MODE.TWO
+  if (game.GameMode == C.GAME_MODE.TWO) then maxPlayers = 4 end
 
-   -- check max number of player
-   if (numberOfPlayers < maxPlayers) then
-      game.Full = false
-   else
-      game.Full = true
-   end
+  -- check max number of player
+  if (numberOfPlayers < maxPlayers) then
+    game.Full = false
+  else
+    game.Full = true
+  end
 
-   -- everyboody left, so we can delete game
-   if (numberOfPlayers == 0) then
-      games[game.Id] = nil
-   else
-      -- we owner is fist player in list
-      game.Owner = allPlayers[1]
-   end
+  -- everyboody left, so we can delete game
+  if (numberOfPlayers == 0) then
+    print("Deleting game")
+    games[game.Id] = nil
+  elseif (not game.Owner) then
+    print("Setting new owner", allPlayers)
+    -- set new owner of game
+    for i, p in pairs(allPlayers) do
+      if (allPlayers[i]) then
+        game.Owner = p
+        break
+      end
+    end
+  end
 
-   print("Server: updateGameStatus", game)
-   return game
+  print("Server: updateGameStatus", game)
+  return game
 end
 
 local function resolveTeamForPlayer(game)
-   local gameMode = game.GameMode
-   local redPlayers = game.Teams[C.GAME_TEAM.RED]
-   local team = nil
-   -- 1vs1
-   if (gameMode == C.GAME_MODE.ONE) then
-      -- 1vs1
-      if (#redPlayers < 1) then
-         team = C.GAME_TEAM.RED
-      else
-         team = C.GAME_TEAM.BLUE
-      end
-   end
+  local gameMode = game.GameMode
+  local redPlayers = game.Teams[C.GAME_TEAM.RED]
+  local team = nil
+  -- 1vs1
+  if (gameMode == C.GAME_MODE.ONE) then
+    -- 1vs1
+    if (#redPlayers < 1) then
+      team = C.GAME_TEAM.RED
+    else
+      team = C.GAME_TEAM.BLUE
+    end
+  end
 
-   -- 2vs2
-   if (gameMode == C.GAME_MODE.TWO) then
-      if (#redPlayers < 2) then
-         team = C.GAME_TEAM.RED
-      else
-         team = C.GAME_TEAM.BLUE
-      end
-   end
-   return team
+  -- 2vs2
+  if (gameMode == C.GAME_MODE.TWO) then
+    if (#redPlayers < 2) then
+      team = C.GAME_TEAM.RED
+    else
+      team = C.GAME_TEAM.BLUE
+    end
+  end
+  return team
 end
 
 local function JoinGame(player, gameId)
-   print("Server: JoinGame", player, gameId)
-   local game = games[gameId]
-   local team = resolveTeamForPlayer(game)
+  print("Server: JoinGame", player, gameId)
+  local game = games[gameId]
+  local team = resolveTeamForPlayer(game)
 
-   -- add player to list
-   table.insert(game.Teams[team], player)
+  -- add player to list
+  table.insert(game.Teams[team], player)
 
-   -- update status of game
-   game = updateGameStatus(game)
+  -- update status of game
+  game = updateGameStatus(game)
 
-   -- set gameId for player
-   player:SetAttribute("gameId", gameId)
-   player:SetAttribute("team", team)
-   player:SetAttribute("ready", false)
+  -- set gameId for player
+  player:SetAttribute("gameId", gameId)
+  player:SetAttribute("team", team)
+  player:SetAttribute("ready", false)
 
-   print("Server: PlayerJoinedGame:", game, player)
-   remoteEvents.PlayerJoinedGame:FireAllClients({ Game = game, Player = player })
-   return gameId
+  print("Server: PlayerJoinedGame:", game, player)
+  remoteEvents.PlayerJoinedGame:FireAllClients({Game = game, Player = player})
+  return gameId
 end
 
 local function LeaveGame(player, gameId)
-   print("Server: LeaveGame", player, gameId)
-   local game = games[gameId]
+  print("Server: LeaveGame", player, gameId)
+  local game = games[gameId]
 
-   local team = player:GetAttribute("team")
-   local playersInTeam = game.Teams[team];
+  local team = player:GetAttribute("team")
+  local playersInTeam = game.Teams[team];
 
-   for i, p in pairs(playersInTeam) do
-		if (p.UserId == player.UserId) then
-         table.remove(playersInTeam, i)
-      end
-	end
+  for i, p in pairs(playersInTeam) do
+    if (p.UserId == player.UserId) then
+      -- if leaving player is owner, set new owner
+      if (p.UserId == game.Owner.UserId) then game.Owner = nil end
+      table.remove(playersInTeam, i)
+    end
+  end
 
-   player:SetAttribute("gameId", nil)
-   player:SetAttribute("team", nil)
-   player:SetAttribute("ready", nil)
+  player:SetAttribute("gameId", nil)
+  player:SetAttribute("team", nil)
+  player:SetAttribute("ready", nil)
 
-   -- update status of game
-   game = updateGameStatus(game)
+  -- update status of game
+  game = updateGameStatus(game)
 
-   remoteEvents.PlayerLeftGame:FireAllClients({ Game = game, Player = player })
+  remoteEvents.PlayerLeftGame:FireAllClients({Game = game, Player = player})
 
-   return true
+  return true
 end
 
 local function CreateGame(player, options)
-   print("Server: CreateGame", options)
-   local gameId = generateGameId(player.UserId);
-   local game = {
-      Id = gameId,
-      GameType = options.GameType,
-      GameMode = options.GameMode,
-      Owner = player,
-      Full = false,
-      Teams = {
-         [C.GAME_TEAM.RED] = {},
-         [C.GAME_TEAM.BLUE] = {},
-      },
-   }
+  print("Server: CreateGame", options)
+  local gameId = generateGameId(player.UserId);
+  local game = {
+    Id = gameId,
+    GameType = options.GameType,
+    GameMode = options.GameMode,
+    Owner = player,
+    Full = false,
+    Teams = {[C.GAME_TEAM.RED] = {}, [C.GAME_TEAM.BLUE] = {}}
+  }
 
-   -- add to game list
-   games[gameId] = game
+  -- add to game list
+  games[gameId] = game
 
-   -- add player to game
-   JoinGame(player, gameId)
+  -- add player to game
+  JoinGame(player, gameId)
 
-   print("Server: Game created", gameId)
+  print("Server: Game created", gameId)
 
-   -- notify all clients, that new game was created
-   remoteEvents.GameCreated:FireAllClients({ Game = game, Player = player })
-   return game
+  -- notify all clients, that new game was created
+  remoteEvents.GameCreated:FireAllClients({Game = game, Player = player})
+  return game
 end
 
-local function GetGame(player, gameId)
-   return games[gameId]
-end;
+local function PlayerReadyChanged(player, options)
+  print("on server", player)
+
+  -- update player attrs
+  player:SetAttribute("ready", options.Ready)
+
+  remoteEvents.PlayerReadyChanged:FireAllClients(options)
+  print("firing on clients", options)
+end
+
+local function GetGame(player, gameId) return games[gameId] end
 
 function GamesService.Exec()
-   print('GamesService.Exec')
-   remoteFunctions.CreateGame.OnServerInvoke = CreateGame
-   remoteFunctions.JoinGame.OnServerInvoke = JoinGame
-   remoteFunctions.LeaveGame.OnServerInvoke = LeaveGame
-   remoteFunctions.ListOfPublicGames.OnServerInvoke = ListOfPublicGames
-   remoteFunctions.GetGame.OnServerInvoke = GetGame
+  print('GamesService.Exec')
+  remoteFunctions.CreateGame.OnServerInvoke = CreateGame
+  remoteFunctions.JoinGame.OnServerInvoke = JoinGame
+  remoteFunctions.LeaveGame.OnServerInvoke = LeaveGame
+  remoteFunctions.ListOfPublicGames.OnServerInvoke = ListOfPublicGames
+  remoteFunctions.GetGame.OnServerInvoke = GetGame
 
-   remoteEvents.PlayerReadyChanged.OnServerEvent:Connect(function(player, options)
-      print("on server", player)
-      
-      -- update player attrs
-      player:SetAttribute("ready", options.Ready)
-
-      remoteEvents.PlayerReadyChanged:FireAllClients(options)
-      print("firing on clients", options)
-   end)
+  remoteEvents.PlayerReadyChanged.OnServerEvent:Connect(PlayerReadyChanged)
 end
 
 return GamesService
