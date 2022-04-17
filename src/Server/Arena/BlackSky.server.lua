efdd907d41013aca18c78b8c770596d25ab8ffed
Josef Šíma <ja@chose.cz>
2022-04-17 14:54:14 +0200

    Create BlackSky.server.lua

---
+++ src/Server/Arena/BlackSky.server.lua
@@ -0,0 +1,13 @@
+-- blur effect
+local lighting = game.Lighting
+
+local sky = Instance.new("Sky")
+sky.Parent = lighting
+sky.MoonTextureId = "rbxassetid://2675785344"
+sky.SkyboxBk = "rbxassetid://2675785344"
+sky.SkyboxDn = "rbxassetid://2675785344"
+sky.SkyboxFt = "rbxassetid://2675785344"
+sky.SkyboxLf = "rbxassetid://2675785344"
+sky.SkyboxRt = "rbxassetid://2675785344"
+sky.SkyboxUp = "rbxassetid://2675785344"
+sky.SunTextureId = "rbxassetid://2675785344"
