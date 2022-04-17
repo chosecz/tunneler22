a10c20a2873f51b9c9c1601a14273e687f01c3a9
KarlitoCZ <pepasimacz@gmail.com>
2022-04-17 13:31:01 +0200

    cameraScript

--- src/Client/Arena/CameraScript.client.lua
+++ src/Client/Arena/CameraScript.client.lua
@@ -1,3 +1,5 @@
+--repeat task.wait() until game.Players.LocalPlayer.Character
+
 local player = game.Players.LocalPlayer
 local camera = workspace.CurrentCamera
 
@@ -5,15 +7,15 @@ player.CharacterAdded:Wait()
 player.Character:WaitForChild("HumanoidRootPart")
 
 camera.CameraSubject = player.Character.HumanoidRootPart
-camera.CameraType = Enum.CameraType.Follow
 camera.FieldOfView = 40
+camera.CameraType = Enum.CameraType.Scriptable
 
 game:GetService('RunService').Stepped:Connect(function()
 	if (player.Character) then
 		local playerPosition = player.Character.HumanoidRootPart.Position
 		local cameraPosition = playerPosition + Vector3.new(-0.1, 100, 0)
 		camera.CFrame = CFrame.new(cameraPosition, playerPosition)
-		camera.Focus = player.Character.HumanoidRootPart.CFrame	
+		camera.Focus = player.Character.HumanoidRootPart.CFrame
 	end
 end)
 
