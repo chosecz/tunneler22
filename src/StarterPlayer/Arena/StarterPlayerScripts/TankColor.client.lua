62197260f3a7867593e2db25770058350b77092d
Josef Šíma <ja@chose.cz>
2022-07-08 14:51:53 +0200

    player is not required here

--- src/StarterPlayer/Arena/StarterPlayerScripts/TankColor.client.lua
+++ src/StarterPlayer/Arena/StarterPlayerScripts/TankColor.client.lua
@@ -1,7 +1,5 @@
 print('Tank script started')
-
 local remoteFunctions = game.ReplicatedStorage:WaitForChild('RemoteFunctions')
-local player = script.Parent
 
 -- call remote function
-remoteFunctions.ColorTank:InvokeServer(player)
+remoteFunctions.ColorTank:InvokeServer()
