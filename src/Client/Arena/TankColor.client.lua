4c14ac8571abcd06346574c7cfaee0986bf398d3
Josef Šíma <ja@chose.cz>
2022-07-08 14:28:27 +0200

    moved

--- src/Client/Arena/TankColor.client.lua
+++ src/Client/Arena/TankColor.client.lua
@@ -1,8 +1,7 @@
 print('Tank script started')
 
 local remoteFunctions = game.ReplicatedStorage:WaitForChild('RemoteFunctions')
-local character = script.Parent
-local player = game.Players:GetPlayerFromCharacter(character)
+local player = script.Parent
 
 -- call remote function
 remoteFunctions.ColorTank:InvokeServer(player)
