68a65a6fe40c7336992ac14577e1768f9ed18927
Josef Šíma <ja@chose.cz>
2023-08-26 21:24:36 +0200

    Enum

--- src/Arena/ReplicatedStorage/Controllers/CameraController.lua
+++ src/Arena/ReplicatedStorage/Controllers/CameraController.lua
@@ -1,16 +1,29 @@
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+local Packages = game:GetService("ReplicatedStorage").Packages
+local Knit = require(Packages.Knit)
+local EnumList = require(Packages.EnumList)
+
+local cameraState = EnumList.new("CameraState", {
+	"FollowPlayer",
+	"SpectatePlayer",
+})
+
 local CameraController = Knit.CreateController {
     Name = "CameraController",
-    State = "FollowPlayer",
+    State = cameraState.FollowPlayer,
     Distance = 40
 }
 
 function CameraController:KnitInit()
     print("CameraController initialized")
+end
+
+function CameraController:KnitStart() 
+    print("CameraController started")
+
     repeat task.wait() until game.Players.LocalPlayer.Character
 
     local player = game.Players.LocalPlayer
-    local character = player.Character or player.CharacterAdded:Wait()
+    local character = player.Character
     local camera = game.Workspace.CurrentCamera
 
     -- Set the camera's CFrame for a top-down view
@@ -33,11 +46,6 @@ function CameraController:KnitInit()
 
     -- Connect the function to be called every frame
     game:GetService("RunService").RenderStepped:Connect(updateCamera)
-end
-
-function CameraController:KnitStart() 
-    print("CameraController started")
-    print("CameraController state: " .. self.State)
 end
 
 return CameraController
