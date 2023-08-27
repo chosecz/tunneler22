60678d8aa8263b24fe2ddad697d9b2e099f32ef2
Josef Šíma <ja@chose.cz>
2023-08-27 18:59:44 +0200

    MoneyService and saving amount to datastore

--- src/Arena/ServerStorage/Services/MoneyService.lua
+++ src/Arena/ServerStorage/Services/MoneyService.lua
@@ -1,6 +1,8 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Knit = require(ReplicatedStorage.Packages.Knit)
 local Players = game:GetService("Players")
+local DataStoreService = game:GetService("DataStoreService")
+local PlayerStore = DataStoreService:GetDataStore("PlayerStore", "Money")
 
 local MoneyService = Knit.CreateService({
   Name = "MoneyService",
@@ -12,7 +14,19 @@ local MoneyService = Knit.CreateService({
 })
 
 function MoneyService:GetMoney(player)
-  return self.MoneyPerPlayer[player] or self.StartingMoney
+  print("Server:GetMoney > userId:" .. player.UserId)
+  local userId = player.UserId
+  local playerMoney = self.MoneyPerPlayer[userId]
+  if playerMoney then
+    return playerMoney
+  end
+
+  local success, savedMoney = pcall(function()
+    return PlayerStore:GetAsync(player.UserId)
+  end)
+
+  -- return money from serveer of
+  return savedMoney or self.StartingMoney
 end
 
 function MoneyService.Client:GetMoney(player)
@@ -20,26 +34,44 @@ function MoneyService.Client:GetMoney(player)
 end
 
 function MoneyService:AddMoney(player, amount)
+  local userId = player.UserId
   local currentMoney = self:GetMoney(player)
   if amount > 0 then
     local newMoney = currentMoney + amount
-    self.MoneyPerPlayer[player] = newMoney
+    self.MoneyPerPlayer[userId] = newMoney
     self.Client.MoneyChanged:Fire(player, newMoney)
   end
+end
+
+function MoneyService:RemovePlayer(player)
+  print("PlayerRemoving...", player)
+  local userId = player.UserId
+  local currentMoney = self.MoneyPerPlayer[userId]
+  if currentMoney and userId then
+    local success, errorMessage = pcall(function()
+      print("Going to save money ".. currentMoney .." for Player:", userId)
+      PlayerStore:SetAsync(userId, currentMoney) 
+    end)
+    if not success then
+      print(errorMessage)
+    end  
+    print("Money saved")
+  end
+  self.MoneyPerPlayer[userId] = nil
 end
 
 function MoneyService:KnitInit()
   print("MoneyService initialized")
   Players.PlayerRemoving:Connect(function(player)
-    self.MoneyPerPlayer[player] = nil
+    self:RemovePlayer(player)
   end)
 end
 
 function MoneyService:KnitStart()
   print("MoneyService started")
-
+  print('Players', Players:GetPlayers())
   while true do
-    wait(1)
+    wait(5)
     for _, player in ipairs(Players:GetPlayers()) do
       self:AddMoney(player, 1)
     end
